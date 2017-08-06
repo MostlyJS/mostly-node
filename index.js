58808c02f3d6b27643bc7f62a15bf26264acc9b9
forafish <fora.luo@gmail.com>
2017-08-06 21:26:42 +0800

    Remove check for dependencies Add plugin help function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 if (!global._babelPolyfill) { require('babel-polyfill'); }
 
 module.exports = require('./lib/index');
+module.exports.plugin = require('./lib/plugin');
---
+++ src/plugin.js
@@ -0,0 +1,22 @@
+import semver from 'semver';
+
+// Check the bare-minimum version of mostly-node
+// Provide consistent interface to register plugins even when the api is changed
+export default function plugin (fn, version) {
+  if (typeof fn !== 'function') {
+    throw new TypeError(`mostly-plugin expects a function, instead got a '${typeof fn}'`);
+  }
+
+  if (version) {
+    if (typeof version !== 'string') {
+      throw new TypeError(`mostly-plugin expects a version string as second parameter, instead got '${typeof version}'`);
+    }
+
+    const mostlyVersion = require('mostly-node/package.json').version;
+    if (!semver.satisfies(mostlyVersion, version)) {
+      throw new Error(`mostly-plugin - expected '${version}' mostly-node version, '${mostlyVersion}' is installed`);
+    }
+  }
+
+  return fn;
+}
