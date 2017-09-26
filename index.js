20ee547862ab010d10c3667564660d84160b80ca
Fora <luoxuemai@boqii.com>
2017-09-26 17:04:13 +0800

    export Errors

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ if (!global._babelPolyfill) { require('babel-polyfill'); }
 
 module.exports = require('./lib/index');
 module.exports.checkPlugin = require('./lib/checkPlugin');
+module.exports.Errors = require('./lib/errors');
