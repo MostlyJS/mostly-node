8cce9823a0651254026800e4744725e757d14c6e
forafish <fora.luo@gmail.com>
2018-04-26 19:12:37 +0800

    remove time log of esm import

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 require = require("esm")(module/*, options*/);
-console.time('mostly-node import');
 module.exports = require("./src/index").default;
 module.exports.checkPlugin = require('./src/checkPlugin').default;
 module.exports.Errors = require('./src/errors').default;
-console.timeEnd('mostly-node import');
