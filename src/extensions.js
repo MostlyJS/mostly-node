a85798bb4c1264d89a76165d5cc315667e1da0ea
forafish <fora.luo@gmail.com>
2018-05-07 02:04:42 +0800

    Fix lint

--- src/extensions.js
+++ src/extensions.js
@@ -183,4 +183,4 @@ export default {
   onServerPreRequest: [onServerPreRequest, onServerPreRequestLoadTest],
   onServerPreHandler: [onServerPreHandler],
   onServerPreResponse: [onServerPreResponse]
-}
+};
